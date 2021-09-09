1d3a3e308f30f4b2335de81e742a7aaf13980e74
timblakel <timblake@udel.edu>
2021-09-09 18:43:09 -0400

    Updating bezier add points

--- easelJS/easelJS_site.js
+++ easelJS/easelJS_site.js
@@ -5,18 +5,28 @@ function init() {
     stage.canvas.height = windowHeight - 20;
     stage.canvas.width = windowWidth - 40;
     stage.update();
+    // Initialize 4 points
     var radius = 10;
     var P1 = fillCircle(radius, windowWidth / 5, windowHeight / 5, "DeepSkyBlue", stage);
     var P2 = fillCircle(radius, windowWidth / 5 * 2, windowHeight / 5 * 3, "DeepSkyBlue", stage);
     var P3 = fillCircle(radius, windowWidth / 5 * 3, windowHeight / 5, "DeepSkyBlue", stage);
     var P4 = fillCircle(radius, windowWidth / 5 * 4, windowHeight / 5 * 4, "DeepSkyBlue", stage);
+    // Draw lines between points
     var points = [P1, P2, P3, P4];
     var lines = new createjs.Shape();
     stage.addChild(lines);
     updateLines(points, lines, stage);
+    // Draw the bezier
     var bezier = new createjs.Shape();
     stage.addChild(bezier);
     updateBezier(points, bezier, stage);
+    // Add points
+    var addPointsButton = document.querySelector("#addPoints");
+    addPointsButton.addEventListener('click', function (event) {
+        addPoints(points, lines, bezier, stage, radius);
+        stage.update();
+    });
+    // Drag functionality
     points.forEach(function (item) {
         item.on("pressmove", function (evt) {
             item.x = evt.stageX;
@@ -57,9 +67,25 @@ function updateLine(P0, P1, line, stage) {
 }
 function updateLines(points, lines, stage) {
     lines.graphics.clear();
-    for (var i = 0; i < 3; i++) {
+    for (var i = 0; i < points.length - 1; i++) {
         var p0 = points[i];
         var p1 = points[i + 1];
         updateLine(p0, p1, lines, stage);
     }
+}
+function addPoints(points, lines, bezier, stage, radius) {
+    var lastPoint = points[points.length - 1];
+    points.push(fillCircle(radius, 5, 5, "DeepSkyBlue", stage));
+    points.push(fillCircle(radius, 7, 7, "DeepSkyBlue", stage));
+    points.push(fillCircle(radius, 9, 9, "DeepSkyBlue", stage));
+    points.slice(-3).forEach(function (item) {
+        item.on("pressmove", function (evt) {
+            item.x = evt.stageX;
+            item.y = evt.stageY;
+            updateBezier(points, bezier, stage);
+            updateLines(points, lines, stage);
+            stage.update();
+        });
+    });
+    updateLines(points, lines, stage);
 }
--- easelJS/easelJS_site.ts
+++ easelJS/easelJS_site.ts
@@ -2,28 +2,39 @@ declare const createjs: any;
 
 function init(){
     
-    let stage = new createjs.Stage("demoCanvas");
-    let windowWidth = window.innerWidth;
-    let windowHeight = window.innerHeight;
+    let stage: any = new createjs.Stage("demoCanvas");
+    let windowWidth: number = window.innerWidth;
+    let windowHeight: number = window.innerHeight;
     stage.canvas.height = windowHeight - 20;
     stage.canvas.width = windowWidth - 40;
     stage.update();
     
+    // Initialize 4 points
     let radius: number = 10;
     let P1 = fillCircle(radius,windowWidth/5,windowHeight/5,"DeepSkyBlue",stage);
     let P2 = fillCircle(radius,windowWidth/5*2,windowHeight/5*3,"DeepSkyBlue",stage);
     let P3 = fillCircle(radius,windowWidth/5*3,windowHeight/5,"DeepSkyBlue",stage);
     let P4 = fillCircle(radius,windowWidth/5*4,windowHeight/5*4,"DeepSkyBlue",stage);
 
+    // Draw lines between points
     let points = [P1, P2, P3, P4];
     let lines = new createjs.Shape();
     stage.addChild(lines);
     updateLines(points,lines,stage);
 
-    let bezier = new createjs.Shape();
+    // Draw the bezier
+    let bezier: any = new createjs.Shape();
     stage.addChild(bezier);
     updateBezier(points,bezier,stage);
 
+    // Add points
+    let addPointsButton: any = document.querySelector("#addPoints");
+    addPointsButton.addEventListener('click', event => {
+        addPoints(points,lines,bezier,stage,radius);
+        stage.update();
+    })
+
+    // Drag functionality
     points.forEach(item => {
         item.on("pressmove", function(evt) {
             item.x = evt.stageX;
@@ -69,9 +80,28 @@ function updateLine(P0,P1,line,stage) {
 
 function updateLines(points,lines,stage) {
     lines.graphics.clear();
-    for (let i = 0; i < 3; i++) {
+    for (let i = 0; i < points.length - 1; i++) {
         let p0 = points[i];
         let p1 = points[i+1];
         updateLine(p0,p1,lines,stage);
     }
+}
+
+function addPoints(points,lines,bezier,stage,radius) {
+    let lastPoint = points[points.length - 1];
+    points.push(fillCircle(radius,5,5,"DeepSkyBlue",stage));
+    points.push(fillCircle(radius,7,7,"DeepSkyBlue",stage));
+    points.push(fillCircle(radius,9,9,"DeepSkyBlue",stage));
+
+    points.slice(-3).forEach(item => {
+        item.on("pressmove", function(evt) {
+            item.x = evt.stageX;
+            item.y = evt.stageY;
+            updateBezier(points,bezier,stage);
+            updateLines(points,lines,stage);
+            stage.update();
+        })
+    })
+
+    updateLines(points,lines,stage);
 }
