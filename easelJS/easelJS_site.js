94390a37037901f025d7ffc6a6e83076bed8dca9
timblakel <timblake@udel.edu>
2021-09-10 01:11:43 -0400

    Updating bezier site, added color changer

--- easelJS/easelJS_site.js
+++ easelJS/easelJS_site.js
@@ -7,35 +7,58 @@ function init() {
     stage.update();
     // Initialize 4 points
     var radius = 10;
-    var P1 = fillCircle(radius, windowWidth / 5, windowHeight / 5, "rgba(46, 46, 213, 0.66)", stage);
-    var P2 = fillCircle(radius, windowWidth / 5 * 2, windowHeight / 5 * 3, "rgba(46, 46, 213, 0.66)", stage);
-    var P3 = fillCircle(radius, windowWidth / 5 * 3, windowHeight / 5, "rgba(46, 46, 213, 0.66)", stage);
-    var P4 = fillCircle(radius, windowWidth / 5 * 4, windowHeight / 5 * 4, "rgba(46, 46, 213, 0.66)", stage);
+    var color = "rgba(46, 46, 213, 0.5)";
+    var P1 = fillCircle(radius, windowWidth / 5, windowHeight / 5, color, stage);
+    var P2 = fillCircle(radius, windowWidth / 5 * 2, windowHeight / 5 * 3, color, stage);
+    var P3 = fillCircle(radius, windowWidth / 5 * 3, windowHeight / 5, color, stage);
+    var P4 = fillCircle(radius, windowWidth / 5 * 4, windowHeight / 5 * 4, color, stage);
     // Draw lines between points
     var points = [P1, P2, P3, P4];
     var lines = new createjs.Shape();
     stage.addChild(lines);
-    updateLines(points, lines, stage);
+    updateLines(points, lines, color, stage);
     // Draw the bezier
+    var bezierRGBA = [255, 255, 255, 1];
+    var bezierColor = 'rgba(${RGBA[0]}, ${RGBA[1]}, ${RGBA[2]}, ${RGBA[3]}';
     var bezier = new createjs.Shape();
     stage.addChild(bezier);
-    updateBezier(points, bezier, stage);
-    // Add points
-    var addPointsButton = document.querySelector("#addPoints");
-    addPointsButton.addEventListener('click', function (event) {
-        addPoints(points, lines, bezier, stage, radius);
-        stage.update();
-    });
+    updateBezier(points, bezier, bezierColor, stage);
     // Drag functionality
     points.forEach(function (item) {
         item.on("pressmove", function (evt) {
             item.x = evt.stageX;
             item.y = evt.stageY;
-            updateBezier(points, bezier, stage);
-            updateLines(points, lines, stage);
+            updateBezier(points, bezier, bezierColor, stage);
+            updateLines(points, lines, color, stage);
             stage.update();
         });
     });
+    // Add points by button
+    var addPointsButton = document.querySelector("#addPoints");
+    addPointsButton.addEventListener('click', function (event) {
+        addPoints(points, lines, bezier, color, bezierColor, stage, radius);
+        stage.update();
+    });
+    // Change color by RGBA values
+    var RGBAInput = [document.querySelector("#R"), document.querySelector("#G"), document.querySelector("#B"), document.querySelector("#Alpha")];
+    for (var i = 0; i < 4; i++) {
+        if (i < 3) {
+            RGBAInput[i].value = "255";
+        }
+        else {
+            RGBAInput[i].value = "1";
+        }
+    }
+    var _loop_1 = function (i) {
+        RGBAInput[i].addEventListener("input", function (event) {
+            bezierRGBA[i] = parseFloat(RGBAInput[i].value);
+            bezierColor = "rgba(" + bezierRGBA[0] + ", " + bezierRGBA[1] + ", " + bezierRGBA[2] + ", " + bezierRGBA[3];
+            updateBezier(points, bezier, bezierColor, stage);
+        });
+    };
+    for (var i = 0; i < 4; i++) {
+        _loop_1(i);
+    }
 }
 function fillCircle(radius, xPos, yPos, color, stage) {
     var circle = new createjs.Shape();
@@ -46,41 +69,46 @@ function fillCircle(radius, xPos, yPos, color, stage) {
     stage.update();
     return circle;
 }
-function updateBezier(points, bezier, stage) {
+function updateBezier(points, bezier, bezierColor, stage) {
     bezier.graphics.clear();
-    var bezierDraw = function (points, bezier, stage) {
+    // Recursively draw bezier through using all points
+    var bezierDraw = function (points, bezierColor) {
         var P1 = points[0];
         var P2 = points[1];
         var P3 = points[2];
         var P4 = points[3];
+        // Draw bezier curve using first 4 points
         bezier.graphics.setStrokeStyle(2);
-        bezier.graphics.beginStroke("#000000");
+        bezier.graphics.beginStroke(bezierColor);
         bezier.graphics.moveTo(P1.x, P1.y);
         bezier.graphics.bezierCurveTo(P2.x, P2.y, P3.x, P3.y, P4.x, P4.y);
         bezier.graphics.endStroke();
+        // Continue the curve with 3 more points after the first 4
         if (points.length >= 6) {
             var pointsSlice = points.slice(3, points.length);
-            bezierDraw(pointsSlice, bezier, stage);
+            bezierDraw(pointsSlice, bezierColor);
         }
         stage.update();
     };
-    bezierDraw(points, bezier, stage);
+    bezierDraw(points, bezierColor);
 }
-function updateLine(P0, P1, line, stage) {
-    line.graphics.setStrokeStyle(2).beginStroke("rgba(46, 46, 213, 0.66)");
+function drawLine(P0, P1, line, color, stage) {
+    // Draw lines between two points
+    line.graphics.setStrokeStyle(2).beginStroke(color);
     line.graphics.moveTo(P0.x, P0.y);
     line.graphics.lineTo(P1.x, P1.y).endStroke();
     stage.update();
 }
-function updateLines(points, lines, stage) {
+function updateLines(points, lines, color, stage) {
+    // Iterate through points and connect with lines
     lines.graphics.clear();
     for (var i = 0; i < points.length - 1; i++) {
         var p0 = points[i];
         var p1 = points[i + 1];
-        updateLine(p0, p1, lines, stage);
+        drawLine(p0, p1, lines, color, stage);
     }
 }
-function addPoints(points, lines, bezier, stage, radius) {
+function addPoints(points, lines, bezier, color, bezierColor, stage, radius) {
     // Make new points in line with previous two points
     var prevTwoP = points.slice(-2);
     var delX = prevTwoP[1].x - prevTwoP[0].x;
@@ -88,22 +116,24 @@ function addPoints(points, lines, bezier, stage, radius) {
     var magnitude = (Math.sqrt(Math.pow(delX, 2) + Math.pow(delY, 2))) * 0.02;
     delX = delX / magnitude;
     delY = delY / magnitude;
+    // Ensure new points are within canvas
     var maxX = stage.canvas.width;
     var maxY = stage.canvas.height;
-    points.push(fillCircle(radius, Math.max(Math.min(prevTwoP[1].x + delX, maxX), 0), Math.max(Math.min(prevTwoP[1].y + delY, maxY), 0), "rgba(46, 46, 213, 0.66)", stage));
-    points.push(fillCircle(radius, Math.max(Math.min(prevTwoP[1].x + delX * 2, maxX), 0), Math.max(Math.min(prevTwoP[1].y + delY * 2, maxY), 0), "rgba(46, 46, 213, 0.66)", stage));
-    points.push(fillCircle(radius, Math.max(Math.min(prevTwoP[1].x + delX * 3, maxX), 0), Math.max(Math.min(prevTwoP[1].y + delY * 3, maxY), 0), "rgba(46, 46, 213, 0.66)", stage));
-    updateBezier(points, bezier, stage);
-    updateLines(points, lines, stage);
+    points.push(fillCircle(radius, Math.max(Math.min(prevTwoP[1].x + delX, maxX), 0), Math.max(Math.min(prevTwoP[1].y + delY, maxY), 0), color, stage));
+    points.push(fillCircle(radius, Math.max(Math.min(prevTwoP[1].x + delX * 2, maxX), 0), Math.max(Math.min(prevTwoP[1].y + delY * 2, maxY), 0), color, stage));
+    points.push(fillCircle(radius, Math.max(Math.min(prevTwoP[1].x + delX * 3, maxX), 0), Math.max(Math.min(prevTwoP[1].y + delY * 3, maxY), 0), color, stage));
+    updateBezier(points, bezier, bezierColor, stage);
+    updateLines(points, lines, color, stage);
     stage.update();
+    // Add drag functionality to new points
     points.slice(-3).forEach(function (item) {
         item.on("pressmove", function (evt) {
             item.x = evt.stageX;
             item.y = evt.stageY;
-            updateBezier(points, bezier, stage);
-            updateLines(points, lines, stage);
+            updateBezier(points, bezier, bezierColor, stage);
+            updateLines(points, lines, color, stage);
             stage.update();
         });
     });
-    updateLines(points, lines, stage);
+    updateLines(points, lines, color, stage);
 }
--- easelJS/easelJS_site.ts
+++ easelJS/easelJS_site.ts
@@ -11,39 +11,60 @@ function init(){
     
     // Initialize 4 points
     let radius: number = 10;
-    let P1 = fillCircle(radius,windowWidth/5,windowHeight/5,"rgba(46, 46, 213, 0.66)",stage);
-    let P2 = fillCircle(radius,windowWidth/5*2,windowHeight/5*3,"rgba(46, 46, 213, 0.66)",stage);
-    let P3 = fillCircle(radius,windowWidth/5*3,windowHeight/5,"rgba(46, 46, 213, 0.66)",stage);
-    let P4 = fillCircle(radius,windowWidth/5*4,windowHeight/5*4,"rgba(46, 46, 213, 0.66)",stage);
+    let color: string = "rgba(46, 46, 213, 0.5)";
+    let P1 = fillCircle(radius,windowWidth/5,windowHeight/5,color,stage);
+    let P2 = fillCircle(radius,windowWidth/5*2,windowHeight/5*3,color,stage);
+    let P3 = fillCircle(radius,windowWidth/5*3,windowHeight/5,color,stage);
+    let P4 = fillCircle(radius,windowWidth/5*4,windowHeight/5*4,color,stage);
 
     // Draw lines between points
     let points = [P1, P2, P3, P4];
     let lines = new createjs.Shape();
     stage.addChild(lines);
-    updateLines(points,lines,stage);
+    updateLines(points,lines,color,stage);
 
     // Draw the bezier
+    let bezierRGBA: number[] = [255, 255, 255, 1];
+    let bezierColor: string = 'rgba(${RGBA[0]}, ${RGBA[1]}, ${RGBA[2]}, ${RGBA[3]}';
     let bezier: any = new createjs.Shape();
     stage.addChild(bezier);
-    updateBezier(points,bezier,stage);
-
-    // Add points
-    let addPointsButton: any = document.querySelector("#addPoints");
-    addPointsButton.addEventListener('click', event => {
-        addPoints(points,lines,bezier,stage,radius);
-        stage.update();
-    })
+    updateBezier(points,bezier,bezierColor,stage);
 
     // Drag functionality
     points.forEach(item => {
         item.on("pressmove", function(evt) {
             item.x = evt.stageX;
             item.y = evt.stageY;
-            updateBezier(points,bezier,stage);
-            updateLines(points,lines,stage);
+            updateBezier(points,bezier,bezierColor,stage);
+            updateLines(points,lines,color,stage);
             stage.update();
         })
     })
+
+    // Add points by button
+    let addPointsButton: any = document.querySelector("#addPoints");
+    addPointsButton.addEventListener('click', event => {
+        addPoints(points,lines,bezier,color,bezierColor,stage,radius);
+        stage.update();
+    })
+
+    // Change color by RGBA values
+    let RGBAInput: HTMLInputElement[] = [document.querySelector("#R"), document.querySelector("#G"), document.querySelector("#B"), document.querySelector("#Alpha")]
+    for (let i = 0; i < 4; i++) {
+        if (i<3) {
+            RGBAInput[i].value = "255";
+        } else {
+            RGBAInput[i].value = "1";
+        }
+    }
+
+    for (let i = 0; i < 4; i++) {
+        RGBAInput[i].addEventListener("input", event => {
+            bezierRGBA[i] = parseFloat(RGBAInput[i].value,);
+            bezierColor = `rgba(${bezierRGBA[0]}, ${bezierRGBA[1]}, ${bezierRGBA[2]}, ${bezierRGBA[3]}`;
+            updateBezier(points,bezier,bezierColor,stage);
+        })
+    }
 }
 
 function fillCircle(radius: number,xPos: number,yPos: number,color: string,stage) {
@@ -55,48 +76,52 @@ function fillCircle(radius: number,xPos: number,yPos: number,color: string,stage
     return circle;
 }
 
-function updateBezier(points,bezier,stage) {
+function updateBezier(points,bezier,bezierColor: string,stage) {
     bezier.graphics.clear();
 
-    let bezierDraw = function (points,bezier,stage) {
+    // Recursively draw bezier through using all points
+    let bezierDraw = function (points,bezierColor) {
         let P1 = points[0];
         let P2 = points[1];
         let P3 = points[2];
         let P4 = points[3];
 
+        // Draw bezier curve using first 4 points
         bezier.graphics.setStrokeStyle(2);
-        bezier.graphics.beginStroke("#000000");
+        bezier.graphics.beginStroke(bezierColor);
         bezier.graphics.moveTo(P1.x,P1.y);
         bezier.graphics.bezierCurveTo(P2.x,P2.y,P3.x,P3.y,P4.x,P4.y);
         bezier.graphics.endStroke();
 
+        // Continue the curve with 3 more points after the first 4
         if (points.length >= 6) {
             let pointsSlice = points.slice(3,points.length);
-            bezierDraw(pointsSlice,bezier,stage);
+            bezierDraw(pointsSlice,bezierColor);
         }
-
         stage.update();
     }
-    bezierDraw(points, bezier, stage);
+    bezierDraw(points,bezierColor);
 }
 
-function updateLine(P0,P1,line,stage) {
-    line.graphics.setStrokeStyle(2).beginStroke("rgba(46, 46, 213, 0.66)");
+function drawLine(P0,P1,line,color: string,stage) {
+    // Draw lines between two points
+    line.graphics.setStrokeStyle(2).beginStroke(color);
     line.graphics.moveTo(P0.x,P0.y);
     line.graphics.lineTo(P1.x,P1.y).endStroke();
     stage.update();
 }
 
-function updateLines(points,lines,stage) {
+function updateLines(points,lines,color,stage) {
+    // Iterate through points and connect with lines
     lines.graphics.clear();
     for (let i = 0; i < points.length - 1; i++) {
         let p0 = points[i];
         let p1 = points[i+1];
-        updateLine(p0,p1,lines,stage);
+        drawLine(p0,p1,lines,color,stage);
     }
 }
 
-function addPoints(points,lines,bezier,stage,radius) {
+function addPoints(points,lines,bezier,color,bezierColor,stage,radius) {
     // Make new points in line with previous two points
     let prevTwoP = points.slice(-2);
     let delX = prevTwoP[1].x - prevTwoP[0].x;
@@ -105,26 +130,28 @@ function addPoints(points,lines,bezier,stage,radius) {
     delX = delX / magnitude;
     delY = delY / magnitude;
     
+    // Ensure new points are within canvas
     let maxX = stage.canvas.width;
     let maxY = stage.canvas.height;
 
-    points.push(fillCircle(radius,Math.max(Math.min(prevTwoP[1].x+delX,maxX),0),Math.max(Math.min(prevTwoP[1].y+delY,maxY),0),"rgba(46, 46, 213, 0.66)",stage));
-    points.push(fillCircle(radius,Math.max(Math.min(prevTwoP[1].x+delX*2,maxX),0),Math.max(Math.min(prevTwoP[1].y+delY*2,maxY),0),"rgba(46, 46, 213, 0.66)",stage));
-    points.push(fillCircle(radius,Math.max(Math.min(prevTwoP[1].x+delX*3,maxX),0),Math.max(Math.min(prevTwoP[1].y+delY*3,maxY),0),"rgba(46, 46, 213, 0.66)",stage));
+    points.push(fillCircle(radius,Math.max(Math.min(prevTwoP[1].x+delX,maxX),0),Math.max(Math.min(prevTwoP[1].y+delY,maxY),0),color,stage));
+    points.push(fillCircle(radius,Math.max(Math.min(prevTwoP[1].x+delX*2,maxX),0),Math.max(Math.min(prevTwoP[1].y+delY*2,maxY),0),color,stage));
+    points.push(fillCircle(radius,Math.max(Math.min(prevTwoP[1].x+delX*3,maxX),0),Math.max(Math.min(prevTwoP[1].y+delY*3,maxY),0),color,stage));
 
-    updateBezier(points,bezier,stage);
-    updateLines(points,lines,stage);
+    updateBezier(points,bezier,bezierColor,stage);
+    updateLines(points,lines,color,stage);
     stage.update();
 
+    // Add drag functionality to new points
     points.slice(-3).forEach(item => {
         item.on("pressmove", function(evt) {
             item.x = evt.stageX;
             item.y = evt.stageY;
-            updateBezier(points,bezier,stage);
-            updateLines(points,lines,stage);
+            updateBezier(points,bezier,bezierColor,stage);
+            updateLines(points,lines,color,stage);
             stage.update();
         })
     })
 
-    updateLines(points,lines,stage);
+    updateLines(points,lines,color,stage);
 }
