7d4b05b878f2d1669d5c2ff673831f2b2a072512
timblakel <timblake@udel.edu>
2021-09-23 00:04:58 -0400

    Basic card rendering

--- TA_Trainer/my-app/src/App.tsx
+++ TA_Trainer/my-app/src/App.tsx
@@ -1,17 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import CARDS from './assets/cards.json';
 import { CardViewer } from './components/CardViewer';
 import { ControlPanel } from './components/ControlPanel';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Container, Row } from 'react-bootstrap';
+import { Card } from './interfaces/card';
 
 function App(): JSX.Element {
+  const [activeCard, setActivecard] = useState<Card>(CARDS[0] as Card);
+
   return (
     <Container className="App">
       <Row>
-        <ControlPanel></ControlPanel>
-        <CardViewer></CardViewer>
+        <ControlPanel setCard={setActivecard}></ControlPanel>
+        <CardViewer card={activeCard}></CardViewer>
       </Row>
     </Container>
   );
--- TA_Trainer/my-app/src/components/CardViewer.tsx
+++ TA_Trainer/my-app/src/components/CardViewer.tsx
@@ -1,5 +1,9 @@
 import { Col } from 'react-bootstrap';
+import { Card } from '../interfaces/card';
 
-export function CardViewer(): JSX.Element {
-    return <Col>Card Viewer</Col>
+export function CardViewer({card}: {card: Card}): JSX.Element {
+    return <Col>
+    <h1>Card Viewer</h1>
+    <div>Current Card: {card.front}</div>
+    </Col>
 }
