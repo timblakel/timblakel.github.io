5d8fc8f456a8eb8ec8abb877bd64e9606d80b8c1
timblakel <timblake@udel.edu>
2021-09-23 00:29:13 -0400

    Bootstrap card

--- TA_Trainer/my-app/src/components/CardViewer.tsx
+++ TA_Trainer/my-app/src/components/CardViewer.tsx
@@ -1,10 +1,19 @@
-import { Col } from 'react-bootstrap';
+import { Col, Card as BootstrapCard } from 'react-bootstrap';
 import { Card } from '../interfaces/card';
 
 export function CardViewer({card}: {card: Card}): JSX.Element {
     return <Col>
-    <h1>Card Viewer</h1>
-    <div>Front: {card.front}</div>
-    <div>Back: {card.back}</div>
+    <BootstrapCard>
+        <BootstrapCard.Body>
+            <BootstrapCard.Title>Card Viewer</BootstrapCard.Title>
+            <BootstrapCard.Text>
+                <div><strong>Front:</strong> {card.front}</div>
+            </BootstrapCard.Text>
+            <BootstrapCard.Text>
+                <div><strong>Back:</strong> {card.back}</div>
+            </BootstrapCard.Text>
+        </BootstrapCard.Body>
+    </BootstrapCard>
+
     </Col>
 }
